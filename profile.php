b5e35586b06dff2269dfd38d95ceef5afa0c0d7a
alehandro <milenovichaleksandar@gmail.com>
2018-01-07 23:38:35 +0100

    like and unlike implemented.

--- profile.php
+++ profile.php
@@ -42,19 +42,54 @@ if (isset($_GET['username'])) {
 
                 if(isset($_POST['post'])){
                     $postbody = $_POST['postbody'];
-                    $userid = Login::isLoggedIn();
+                    $loggedInUserId = Login::isLoggedIn();
 
                     if(strlen($postbody) > 160 || strlen($postbody)<1){
                       die('Incorrect length!');
                     }
 
+                    if($loggedInUserId == $userid){
+
                     DB::query('INSERT INTO posts VALUES (\'\',:postbody,NOW(),:userid,0)', array(':postbody' => $postbody,':userid'=>$userid ));
+                  }else {
+                    echo die('Incorrect user!!!');
+                  }
+                }
+
+                if(isset($_GET['postid'])){
+                  if(!DB::query('SELECT user_id FROM post_likes WHERE post_id=:postid AND user_id=:userid',array(':postid' => $_GET['postid'],':userid'=>$followerid))){
+                    DB::query('UPDATE posts SET likes=likes+1 WHERE id=:postid',array(':postid'=>$_GET['postid']));
+                    DB::query('INSERT INTO post_likes VALUES (\'\',:postid,:userid)',array(':postid'=>$_GET['postid'],':userid'=>$followerid));
+                  }else {
+                    echo "Already liked";
+                    DB::query('UPDATE posts SET likes=likes-1 WHERE id=:postid',array(':postid'=>$_GET['postid']));
+                    DB::query('DELETE FROM post_likes WHERE post_id=:postid AND user_id=:userid',array(':postid'=>$_GET['postid'],':userid'=>$followerid));
+                  }
+
                 }
 
                 $dbpost = DB::query('SELECT * FROM posts WHERE user_id=:userid ORDER BY id DESC', array(':userid' => $userid));
                 $posts = "";
                 foreach ($dbpost as $p) {
-                  $posts .= $p['body']."<hr/></br />";
+
+                if(!DB::query('SELECT post_id FROM post_likes WHERE post_id=:postid AND user_id=:userid',array(':postid'=>$p['id'],':userid'=>$followerid))){
+
+                  $posts .= htmlspecialchars($p['body'])."
+                  <form action='profile.php?username=$username&postid=".$p['id'].";' method='post'>
+                     <input type='submit' name='like' value='Like'>
+                     <span>".$p['likes']."likes</span>
+                  </form>
+
+                  <hr/></br />";
+                }else {
+                  $posts .= htmlspecialchars($p['body'])."
+                  <form action='profile.php?username=$username&postid=".$p['id'].";' method='post'>
+                     <input type='submit' name='unlike' value='Unike'>
+                     <span>".$p['likes']."likes</span>
+                  </form>
+
+                  <hr/></br />";
+                }
                 }
 
 
@@ -80,6 +115,7 @@ if (isset($_GET['username'])) {
   <textarea name="postbody" rows="8" cols="80"></textarea>
   <input type="submit" name="post" value="Post">
 </form>
+
 
 
 <div class="posts">
