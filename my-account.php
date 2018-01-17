69240796315945542350ce13119a04ddfa0d8daa
alehandro <milenovichaleksandar@gmail.com>
2018-01-17 02:26:52 +0100

    upload images added.

--- my-account.php
+++ my-account.php
@@ -1,5 +1,13 @@
 <?php
+include('./classes/DB.php');
+include('./classes/Login.php');
 
+if(Login::isLoggedIn()){
+$userid = Login::isLoggedIn();
+}else {
+  die('Not logged in!!!');
+
+}
 
 
 if(isset($_POST['uploadprofileimg'])){
@@ -20,7 +28,19 @@ if(isset($_POST['uploadprofileimg'])){
 
     $imgurURL = "https://api.imgur.com/3/image";
 
+    if($_FILES['profileimg']['size'] > 10240000){
+      die('Image too big, must be 10MB or less!');
+    }
+
     $response = file_get_contents($imgurURL,false,$context);
+    $response = json_decode($response);
+    echo '<pre>';
+    print_r($response);
+    echo '</pre>';
+    echo $response->data->link;
+
+    DB::query("UPDATE users SET profileimg = :profileimg WHERE id = :userid",array(':profileimg'=>$response->data->link,':userid'=>$userid));
+
 }
 
  ?>
