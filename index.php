e86d3e548480f093f9591fc6bab59b9ab0b28922
alehandro <milenovichaleksandar@gmail.com>
2018-01-20 23:56:05 +0100

    search added.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include('./classes/DB.php');
 include('./classes/Login.php');
 include('./classes/Post.php');
 include('./classes/Comment.php');
+include('./classes/Notify.php');
 $showTimeline = False;
 
 
@@ -25,7 +26,49 @@ if (isset($_GET['comment'])) {
   Comment::createComment($_POST['commentbody'],$_GET['comment'],$userid);
 }
 
+if(isset($_POST['search'])){
+    $tosearch =explode(" ",$_POST['searchbox']);
+    if(count($tosearch) == 1){
+      $tosearch = str_split($tosearch[0],2);
+    }
 
+    $whereckause = "";
+    $paramsarray = array(':username' => '%'.$_POST['searchbox'].'%');
+
+    for ($i=0; $i < count($tosearch); $i++) {
+
+        $whereckause .= " OR username LIKE :u$i ";
+        $paramsarray[":u$i"] = $tosearch[$i];
+    }
+
+
+
+    $users = DB::query('SELECT users.username FROM users WHERE users.username LIKE :username '.$whereckause.'',$paramsarray);
+    print_r($users);
+
+    $whereckause = "";
+    $paramsarray = array(':body' => '%'.$_POST['searchbox'].'%');
+
+    for ($i=0; $i < count($tosearch); $i++) {
+        if($i%2 == 0){
+        $whereckause .= " OR body LIKE :p$i ";
+        $paramsarray[":p$i"] = $tosearch[$i];
+      }
+    }
+
+    $posts = DB::query('SELECT posts.body FROM posts WHERE posts.body LIKE :body '.$whereckause.'',$paramsarray);
+    echo "<pre>";
+    print_r($posts);
+    echo "</pre>";
+  }
+
+?>
+<form  action="index.php" method="post">
+<input type="text" name="searchbox" value="">
+<input type="submit" name="search" value="Search">
+</form>
+
+<?php
 $followingposts = DB::query('SELECT posts.id,posts.body,posts.likes,users.username FROM users,posts,followers
 WHERE posts.user_id = followers.user_id
 AND users.id = posts.user_id
